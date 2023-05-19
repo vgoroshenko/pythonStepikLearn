df31b3765affe580f7706984513ee68bad538660
vgoroshenko <vgoroshenko1@gmail.com>
2023-05-20 02:04:42 +0600

    fix 8.3, add 8.4

--- 3st_cours/8/8.3.py
+++ 3st_cours/8/8.3.py
@@ -35,11 +35,88 @@
 # print(number_of_frogs(10))
 
 
-def range_sum(numbers, start, end):
-    if start == end:
-        return numbers[start]
-    else:
-        return numbers[start] + range_sum(numbers, start + 1, end)
+# def range_sum(numbers, start, end):
+#     if start == end:
+#         return numbers[start]
+#     else:
+#         return numbers[start] + range_sum(numbers, start + 1, end)
+#
+#
+# print(range_sum([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 7))
 
 
-print(range_sum([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 7))
+# def get_pow(a, n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return a * get_pow(a, (n - 1))
+#
+# print(get_pow(2, 10))
+
+
+# def get_fast_pow(a, n):
+#     if n == 0:
+#         return 1
+#     else:
+#         if n % 2 == 0:
+#             return get_fast_pow(a * a, n / 2)
+#         else:
+#             return a * get_fast_pow(a, n - 1)
+#
+#
+# print(get_fast_pow(2, 10))
+# print(get_fast_pow(5, 2))
+# print(get_fast_pow(2, 100))
+
+
+# def recursive_sum(a, b):
+#     if b == 0:
+#         return 0
+#     else:
+#         return a + recursive_sum(a + 1, b - 1)
+#
+#
+# print(recursive_sum(10, 22))
+# print(recursive_sum(99, 0))
+# print(recursive_sum(0, 0))
+
+
+# def is_power(number: int, ch = 2) -> bool:
+#     if number == 1 or number == ch:
+#         return True
+#     else:
+#         if ch <= number:
+#             return is_power(number, ch*2)
+#         else:
+#             return False
+#
+# print(is_power(512))
+# print(is_power(15))
+
+
+cache = {1: 1, 2: 1, 3: 1}
+
+
+# def tribonacci(n):
+#     res = cache.get(n)
+#     if res is None:
+#         res = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1)
+#         cache[n] = res
+#     return res
+#
+# print(tribonacci(7))
+# print(tribonacci(4))
+
+
+#def is_palindrome(string):
+
+
+
+
+
+
+
+
+
+
+
---
+++ 3st_cours/8/8.4.py
@@ -0,0 +1,67 @@
+asd = 0
+# def recursive_sum(nested_list, total=0):
+#     if not nested_list:
+#         return 0
+#     for i in nested_list:
+#         if isinstance(i, int):
+#             total += i
+#         else:
+#             total += recursive_sum(i)
+#     return total
+#
+# my_list = [1, [4, 4], 2, [1, [2, 10]]]
+#
+# print(recursive_sum(my_list))
+
+
+# def linear(li):
+#     new_list = []
+#     for elem in li:
+#         if isinstance(elem, list):
+#             new_list.extend(linear(elem))
+#         else:
+#             new_list.append(elem)
+#     return new_list
+#
+#
+# my_list = [3, [4], [5, [6, [7, 8]]]]
+#
+# print(linear(my_list))
+
+
+# def get_value(nested_dict, key):
+#     if key in nested_dict:
+#         return nested_dict[key]
+#     for v in nested_dict.values():
+#         if type(v) == dict:
+#             value = get_value(v, key)
+#             if value is not None:
+#                 return value
+#
+# data = {'firstName': 'Тимур', 'lastName': 'Гуев', 'birthDate': {'day': 10, 'month': 'October', 'year': 1993}, 'address': {'streetAddress': 'Часовая 25, кв. 127', 'city': {'region': 'Московская область', 'type': 'город', 'cityName': 'Москва'}, 'postalCode': '125315'}}
+#
+# print(get_value(data, 'cityName'))
+
+def get_all_values(nested_dict, key):
+    myset = set()
+    if key in nested_dict:
+        return nested_dict[key]
+    for v in nested_dict.values():
+        if type(v) == dict:
+            value = get_all_values(v, key)
+            if value is not None:
+                myset.add(value)
+    return myset
+
+# my_dict = {'users': {'Arthur': {'grades': [4, 4, 3], 'top_grade': 4}, 'Timur': {'grades': [5, 5, 5], 'top_grade': 5}}}
+# result = get_all_values(my_dict, 'top_grade')
+#
+# print(*sorted(result))
+
+my_dict = {'Arthur': {'hobby': 'videogames', 'drink': 'cacao'}, 'Timur': {'hobby': 'math'}}
+result = get_all_values(my_dict, 'top_grade')
+
+print(len(sorted(result)))
+
+
+
